c09467a67dbbaa189aeffb6a56b2580e4e6488fb
Halil kakut <halil.kakut@hotmail.com>
2021-01-05 22:16:55 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import {appRoutes} from './routes'
+import { NgxGalleryModule } from '@kolkov/ngx-gallery'
 
 import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+import { CityComponent } from './city/city.component';
+import {CityDetailComponent} from './city/city-detail/city-detail.component';
+
+import { from } from 'rxjs';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [			
+    AppComponent,
+      ValueComponent,
+      NavComponent,
+      CityComponent,
+      CityDetailComponent
+   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes),
+    NgxGalleryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { CityDetailComponent } from './city/city-detail/city-detail.component';
+import { CityComponent } from './city/city.component';
+import { ValueComponent } from './value/value.component';
+
+export const appRoutes : Routes = [
+    {path:"city",component:CityComponent},
+    {path:"value",component:ValueComponent},
+    {path:"cityDetail/:cityId",component:CityDetailComponent},
+    {path:"**",redirectTo:"city", pathMatch:"full"}
+];
+
+
